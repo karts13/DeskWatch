1f65e812564615842e92b248303296e1ff338229
Karthik <karthishenoy2014@gmail.com>
2024-03-02 23:36:28 +0530

    Tracks websites visited

--- deskwatch.py
+++ deskwatch.py
@@ -1,12 +1,22 @@
 import pygetwindow as gw
 import time
+import pyautogui
+import pyperclip
 
-active = ""
+active_window = ""
+
 while True:
-    new = gw.getActiveWindow().title
+    current_window = gw.getActiveWindow().title
 
-    if active != new:
-        active = new
-        print(active)
+    if current_window != active_window:
+        active_window = current_window
+        print(active_window)
+
+        if "Brave" in active_window:
+            pyautogui.hotkey('ctrl', 'l')
+            pyautogui.hotkey('ctrl', 'c')
+            time.sleep(1)  # Wait for the clipboard to be updated
+            url = pyperclip.paste()       
+            print(url)
 
     time.sleep(10)
