23effad83a9dd773e75e2e4beb701c4e39c3c473
A Karthik Shenoy <126340629+karts13@users.noreply.github.com>
2024-03-18 23:10:23 +0530

    Timer works perfectly

--- deskwatch.py
+++ deskwatch.py
@@ -2,42 +2,43 @@ import pygetwindow as gw
 import time
 import pyautogui
 import pyperclip
-from datetime import datetime, timedelta
+import datetime
 
 active_window = ""
-start_time = None
+activity_name = ""
+start_time = datetime.datetime.now()
+first_time = True
 
-try:
+def url_to_name(url):
+    string_list = url.split('/')
+    return string_list[2]
+
+try: 
     while True:
         current_window = gw.getActiveWindow().title
-        current_time = datetime.now()
-
-        if current_window != active_window:
-            if active_window:
-                end_time = datetime.now()
-                time_spent = end_time - start_time
-
-                print("Activity:", active_window)
-                print("Time Spent:", time_spent)
-                print("Time Entry Details:")
-                print("Start Time:", start_time)
-                print("End Time:", end_time)
-                print("Total Hours:", time_spent.total_seconds() // 3600)
-                print("Total Minutes:", (time_spent.total_seconds() % 3600) // 60)
-                print("Total Seconds:", time_spent.total_seconds())
-                print("\n")
-
-            active_window = current_window
-            start_time = datetime.now()
 
         if "Brave" in active_window:
             pyautogui.hotkey('ctrl', 'l')
             pyautogui.hotkey('ctrl', 'c')
-            time.sleep(1)
-            url = pyperclip.paste()
-            print("URL:", url)
+            time.sleep(1)  # Wait for the clipboard to be updated
+            url = pyperclip.paste()       
+            print(url)
+            current_window = url_to_name(url)
 
-        time.sleep(10)
+        if current_window != active_window:
+            print(active_window)
+            activity_name = active_window
+
+            if not first_time:
+                end_time = datetime.datetime.now()
+                time_spent = end_time - start_time
+                print(f"Time spent on {activity_name}: {time_spent}")
+                start_time = datetime.datetime.now()
+
+            first_time = False
+            active_window = current_window
+
+        time.sleep(1)
 
 except KeyboardInterrupt:
-    print("Tracking stopped.")
+    pass
